aceeb3ad6dc06abc4ee742c04afe02890473d1a5
os <os@os-VirtualBox.(none)>
2016-03-18 11:03:29 +0800

    ???

--- PJ1/show.c
+++ PJ1/show.c
@@ -2,6 +2,6 @@
 #include <linux/linkage.h>
 
 asmlinkage int sys_show(void) {
-	printk("B03902024 Hsiao-Hua Cheng, B03902084 Yi-Lin Wang!!\n");
+	printk("B03902024 Hsiao-Hua Cheng, B03902084 Yi-Lin Wang!!!!\n");
 	return 0;
 }
